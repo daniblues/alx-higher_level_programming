01aed3340dcd3d3a1737401d0c84cb5e35924be6
Daniel <amaize345@gmail.com>
2022-09-27 06:21:01 -0700

    correct the code

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
-def number_of_lines(filename=""):
-    count = 0
-    with open(filename, encoding="utf-8") as fd:
-        for line in fd:
-            count += 1
-    return count
+""" Module that contains a function that writes to a text file
+"""
+
+
+def write_file(filename="", text=""):
+    """ Function that writes to a text file
+
+    Args:
+        filename: filename
+        text: text to write
+
+    Raises
+        Exception: when the file can be opened
+
+    """
+
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
