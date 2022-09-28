892568cb1c5753841ec9a5b5b4de9de417d732a8
Daniel <amaize345@gmail.com>
2022-09-28 06:04:06 -0700

    correction

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,36 @@
 #!/usr/bin/python3
-import json
+"""
+    6-from_json_string.py
+    Function that writes an Object to \
+    a text file, using a JSON representation.
+"""
 import sys
-import os.path
+import json
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+def create_file_list():
+    """Adds all arguments to a Python list, and then save them to a file"""
+    obj_9 = load_from_json_file("add_item.json")
 
-filename = "add_item.json"
-if os.path.isfile(filename):
-    obj = load_from_json_file(filename)
+    for i in range(1, len(sys.argv)):
+        obj_9.append(str(sys.argv[i]))
+
+    save_to_json_file(obj_9, "add_item.json")
+
+
+if len(sys.argv) < 2:
+    try:
+        f = open("add_item.json")
+        f.close()
+    except IOError:
+        save_to_json_file([], "add_item.json")
 else:
-    obj = []
-obj.extend(sys.argv[1:])
-save_to_json_file(obj, filename)
+    try:
+        f = open("add_item.json")
+        f.close()
+        create_file_list()
+    except IOError:
+        save_to_json_file([], "add_item.json")
+        create_file_list()
