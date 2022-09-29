dd0a55084f18fc605108b01428b396cee1eb592f
Daniel <amaize345@gmail.com>
2022-09-28 19:22:36 -0700

    correction

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -5,16 +5,15 @@
 class student:
     """ class Student """
 
-    def __init__(self, first_name, last_name, age):
-        """ defines a student by
-        first name, last name and age"""
 
+    def __init__(self, first_name, last_name, age):
+        """ Special method to initialize """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """ Public method that retrieves a dictionary
-        representation of a Student """
+        """ Method that returns directory description """
+        return self.__dict__.copy()
 
-        return(self.__dict__)
+
