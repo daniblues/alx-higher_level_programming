600cc1d5f0804d7d24788d1252380b76d82a2052
Daniel <amaize345@gmail.com>
2022-09-28 20:41:37 -0700

    correct it

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
-"""Reads from standard input and computes metrics.
-After every ten lines or the input of a keyboard interruption (CTRL + C),
-prints the following statistics:
-    - Total file size up to that point.
-    - Count of read status codes up to that point.
-"""
+"""Contains a stdin"""
 
 
 def print_stats(size, status_codes):
     """Print accumulated metrics.
-    Args:
-        size (int): The accumulated read file size.
-        status_codes (dict): The accumulated count of status codes.
     """
     print("File size: {}".format(size))
     for key in sorted(status_codes):
@@ -50,8 +42,8 @@ if __name__ == "__main__":
             except IndexError:
                 pass
 
-    print_stats(size, status_codes)
+        print_stats(size, status_codes)
 
-except KeyboardInterrupt:
-    print_stats(size, status_codes)
-    raise
+    except KeyboardInterrupt:
+        print_stats(size, status_codes)
+        raise
