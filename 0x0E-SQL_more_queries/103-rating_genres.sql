d07e81e5e99e8df5104ed7ec8adda71f89874130
Daniel Amaize <amaize345@email.com>
2023-02-16 02:13:37 +0100

    corrected code

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,7 +1,9 @@
--- Script that lists shows by their rating
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating -- Query to join tables
-FROM tv_shows
-JOIN tv_show_ratings
-     ON tv_show_ratings.show_id = tv_shows.id
-GROUP BY tv_shows.title
+-- List all genres in 'hbtn_0d_tvshows_rate' by their rating
+-- Each record should display tv_genres.name and rating sum
+-- You can use only one SELECT statement
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY tv_show_genres.genre_id
 ORDER BY rating DESC;
