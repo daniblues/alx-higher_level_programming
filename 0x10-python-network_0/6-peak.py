8edb1376b18eeb7eddb563ca2ef44556c1ab8c56
Daniel Amaize <amaize345@email.com>
2023-03-30 23:10:57 +0100

    corrections to task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,31 +1,25 @@
 #!/usr/bin/python3
 """
-Function that finds a peak in a list of unsorted integers
+a function that finds a peak in a list of unsorted integers.
 """
 
 
 def find_peak(list_of_integers):
     """
-    finds num that's greater than both left and right
+    Return a peak in a list of unsorted integers
     """
     if len(list_of_integers) == 0:
         return None
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+    if len(list_of_integers) == 2:
+        return max(list_of_integers)
 
-    lists = list_of_integers
-    beg = 0
-    end = len(lists)-1
-
-    if lists[beg] > lists[beg+1]:
-        return lists[beg]
-    if lists[end] > lists[end-1]:
-        return lists[end]
-
-    mid = (beg+end)//2
-    if lists[mid-1] < lists[mid] and lists[mid+1] < lists[mid]:
-        return lists[mid]
-    if lists[mid] < lists[mid-1]:
-        return find_peak(lists[beg:mid+1])
-    elif lists[mid] < lists[mid+1]:
-        return find_peak(lists[mid:end+1])
+    mid = int(len(list_of_integers) / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
     else:
-        return lists[beg]
+        return find_peak(list_of_integers[mid + 1:])
