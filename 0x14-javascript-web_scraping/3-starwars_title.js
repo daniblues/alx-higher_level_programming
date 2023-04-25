7a233aa7fe9191004ee400796e95bd212c713d77
daniblues <you@amaize345@gmail.com>
2023-04-25 12:26:45 -0700

    task 3 correction

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-// JS Script
-require('request').get('http://swapi.co/api/films/' + process.argv[2] + '/', function (err, r, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(JSON.parse(body).title);
+const request = require('request');
+const id = process.argv[2];
+request('http://swapi.co/api/films/' + id + '/', function (error, response, body) {
+  if (error == null) {
+    const json = JSON.parse(body);
+    console.log(json.title);
   }
 });
